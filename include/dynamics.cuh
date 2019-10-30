946a8439538f31e2bc0df7e5026db39a9514e3f6
Wang_Ziyi_zwang450 <zwang450@ae-acds-405028.ae.gatech.edu>
2019-10-30 14:32:45 -0400

    finished cartpole dynamics, untested

--- include/dynamics.cuh
+++ include/dynamics.cuh
@@ -3,6 +3,8 @@ Header file for dynamics
 */
 
 #include <Eigen/Dense>
+#include <stdio.h>
+#include <math.h>
 
 template<int S_DIM, int C_DIM>
 class Dynamics
@@ -10,21 +12,19 @@ class Dynamics
 public:
   static const int STATE_DIM = S_DIM;
   static const int CONTROL_DIM = C_DIM;
+  Eigen::Matrix<float, STATE_DIM, 1> state_der_;
   float dt;
 
   Dynamics();
   ~Dynamics();
 
-  void xDot(Eigen::MatrixXf &state, Eigen::MatrixXf &control); //passing values in by reference
+  void xDot(Eigen::MatrixXf &state, Eigen::MatrixXf &control, Eigen::MatrixXf &state_der); //passing values in by reference
   void computeGrad(Eigen::MatrixXf &state, Eigen::MatrixXf &control); //compute the Jacobians with respect to state and control
   void loadParams(); //figure out what to pass in here
   void paramsToDevice();
   void freeCudaMem();
   void printState();
   void printParams();
-  void incrementState(Eigen::MatrixXf &state, Eigen::MatrixXf &control);
 
-  __device__ void xDot(float* state, float* control);
-  __device__ void computeGrad(float* state, float* control);
-  __device__ void incrementState(float* state, float* control);
+  __device__ void xDot(float* state, float* control, float* state_der);
 }
