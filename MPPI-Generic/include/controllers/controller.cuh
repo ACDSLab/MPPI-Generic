6a4317b489fddb69042d7486fde2e96a1e0546e4
Jason Gibson <jgibson37@gatech.edu>
2019-11-02 19:34:22 -0400

    started basic template setup

---
+++ MPPI-Generic/include/controllers/controller.cuh
@@ -0,0 +1,12 @@
+//
+// Created by jason on 10/30/19.
+//
+
+#ifndef MPPIGENERIC_CONTROLLER_CUH
+#define MPPIGENERIC_CONTROLLER_CUH
+
+class Controller {
+
+}
+
+#endif //MPPIGENERIC_CONTROLLER_CUH
---
+++ MPPI-Generic/include/mppi_core/mppi_controller.cuh
@@ -0,0 +1,12 @@
+//
+// Created by jason on 10/30/19.
+//
+
+#ifndef MPPIGENERIC_MPPI_CONTROLLER_CUH
+#define MPPIGENERIC_MPPI_CONTROLLER_CUH
+
+class MPPIController {
+
+}
+
+#endif //MPPIGENERIC_MPPI_CONTROLLER_CUH
