41a73b3eea8b1f8ce4f415e6ea841b2105e323e6
Manan Gandhi <mgandhi3@gatech.edu>
2019-11-18 15:37:26 -0500

    Add support for gtest building.
    TODO gtest does not play well with CUDA at the moment, we will likely have to set it up so that we can link to MPPI and its related libraries (i.e dynamics) without using nvcc.

---
+++ tests/cartpole_dynamics_tests.cpp
@@ -0,0 +1,11 @@
+//
+// Created by mgandhi3 on 10/4/19.
+//
+
+// #include "cartpole.cuh"
+#include <gtest/gtest.h>
+
+TEST(Dummy, One) {
+    std::cout << "Hello Testing!" << std::endl;
+    FAIL();
+}
---
+++ tests/test_main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by mgandhi3 on 11/18/19.
+//
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
