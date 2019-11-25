3da59f5e8b5b90acd33f12808dd72cdf9c5a1f7a
Manan Gandhi <mgandhi3@gatech.edu>
2019-11-25 17:00:36 -0500

    Add dynamics equilibrium test.

--- tests/cartpole_dynamics_tests.cu
+++ tests/cartpole_dynamics_tests.cu
@@ -6,10 +6,34 @@
 #include "cartpole.cuh"
 
 
-TEST(Dummy, One) {
+TEST(CartPole, StateDim) {
     auto CP = Cartpole(0.1, 1, 1, 1);
     EXPECT_EQ(4, Cartpole::STATE_DIM);
 }
 
-TEST(Dummy, two) {
+TEST(CartPole, ControlDim) {
+    auto CP = Cartpole(0.1,1,1,1);
+    EXPECT_EQ(1, Cartpole::CONTROL_DIM);
 }
+
+TEST(CartPole, Equilibrium) {
+    auto CP = Cartpole(0.1,1,1,1);
+    
+    Eigen::MatrixXf state(Cartpole::STATE_DIM,1);
+    state << 0,0,0,0;
+    
+    Eigen::MatrixXf control(Cartpole::CONTROL_DIM,1);
+    control << 0;
+
+    Eigen::MatrixXf state_dot_compute(Cartpole::STATE_DIM,1);
+    state_dot_compute << 1,1,1,1;
+
+    Eigen::MatrixXf state_dot_known(Cartpole::STATE_DIM,1);
+    state_dot_known << 0,0,0,0;
+
+    CP.xDot(state, control, state_dot_compute);
+    for (int i = 0; i < Cartpole::STATE_DIM; i++) {
+        EXPECT_NEAR(state_dot_known(i), state_dot_compute(i), 1e-4);
+    }
+}
+
