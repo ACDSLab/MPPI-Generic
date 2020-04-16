65b20d3dec9aa1a2a87dd4622c5b535af461d2a2
Jason Gibson <jgibson37@gatech.edu>
2020-04-16 16:16:54 -0400

    moved mock classes to own files and created test include directory

---
+++ tests/controllers/controller_generic_tests.cu
@@ -0,0 +1,13 @@
+#include <mppi/controllers/controller.cuh>
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <mppi/utils/test_helper.h>
+#include <random>
+#include <algorithm>
+#include <numeric>
+
+
+TEST(Controller, Constructor) {
+  EXPECT_EQ(1, 1);
+}
---
+++ tests/include/mppi_test/mock_classes/mock_dynamics.h
@@ -0,0 +1,23 @@
+//
+// Created by jason on 4/14/20.
+//
+
+#ifndef MPPIGENERIC_MOCK_DYNAMICS_H
+#define MPPIGENERIC_MOCK_DYNAMICS_H
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <mppi/dynamics/dynamics.cuh>
+
+// ===== mock dynamics ====
+typedef struct {
+  int test = 2;
+} mockDynamicsParams;
+
+class MockDynamics : public Dynamics<MockDynamics, mockDynamicsParams, 1, 1> {
+public:
+  MOCK_METHOD1(bindToStream, void(cudaStream_t stream));
+  MOCK_METHOD1(setParams, void(mockDynamicsParams params));
+};
+
+#endif //MPPIGENERIC_MOCK_DYNAMICS_H
