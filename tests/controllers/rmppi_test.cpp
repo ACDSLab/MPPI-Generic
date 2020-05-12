61751df7e77be8cbf7e94b788071363193a82cf7
Bogdan Vlahov <bvlahov3@gatech.edu>
2020-05-12 18:25:02 -0400

    Start CPU RMPPI rolloutKernel
    Still needs to fill in some components such as the initial trajectories, noise, and writing out the cost functions component but it is getting closer

--- tests/controllers/rmppi_test.cpp
+++ tests/controllers/rmppi_test.cpp
@@ -4,10 +4,82 @@
 TEST(RMPPITest, CPURolloutKernel) {
   DoubleIntegratorDynamics model;
   DoubleIntegratorCircleCost cost;
+
+  const int state_dim = DoubleIntegratorDynamics::STATE_DIM;
+  const int control_dim = DoubleIntegratorDynamics::CONTROL_DIM;
+  typedef Eigen::Matrix<float, control_dim, state_dim> K_M;
+
   float dt = 0.01;
   int max_iter = 10;
   float gamma = 0.5;
   const int num_timesteps = 100;
+  const int num_rollouts = 5;
+
+  float x[num_rollouts * state_dim * 2];
+  float x_dot[num_rollouts * state_dim * 2];
+  float u[num_rollouts * control_dim * 2];
+  float du[num_rollouts * control_dim * 2];
+  float sigma_u[control_dim]; // variance to sample noise from
+  float fb_u[num_rollouts * control_dim];
+
+  // Generate control noise
+  float noisey_sample[num_rollouts * num_timesteps * control_dim * 2];
+  // Initial control trajectory
+  float u_traj[num_rollouts * num_timesteps * control_dim * 2];
+
+  VanillaMPPIController<DoubleIntegratorDynamics, DoubleIntegratorCircleCost, 100, 512, 64, 8>::feedback_gain_trajectory feedback_gains;
+  for (int i = 0; i < 10; i++) {
+    feedback_gains.push_back(K_M::Random());
+  }
+
+  float feedback_array[num_rollouts * num_timesteps * control_dim * state_dim * 2];
+  for (size_t i = 0; i < feedback_gains.size(); i++) {
+    std::cout << "Matrix " << i << ":\n";
+    std::cout << feedback_gains[i] << std::endl;
+  }
+
+  for (int traj_i = 0; traj_i < num_rollouts; traj_i++)  {
+    float cost_real_w_tracking = 0; // S^(V, x_0, x*_0) in Grady Thesis (8.24)
+    float cost_real = 0; // S(V, x_0) with knowledge of tracking controller
+    float cost_nom = 0; // S(V, x*_0)
+
+    int traj_index = traj_i * num_rollouts;
+
+     // Get all relevant values at time t in rollout i
+    DoubleIntegratorDynamics::state_array x_t_nom;
+    DoubleIntegratorDynamics::state_array x_t_act;
+    // Eigen::Map<DoubleIntegratorDynamics::state_array> x_t_act(x + traj_index * state_dim);
+    for (int state_i = 0; state_i < state_dim; state_i++) {
+      x_t_act(state_i, 0) = x[traj_index * state_dim + state_i];
+      x_t_nom(state_i, 0) = x[(traj_index + num_rollouts) * state_dim + state_i];
+    }
+
+    for (int control_i = 0; control_i < control_dim; ) {
+
+    }
+    for (int t = 0; t < num_timesteps - 1; t++){
+      // Controls are read only so I can use Eigen::Map
+      Eigen::Map<DoubleIntegratorDynamics::control_array>
+        u_t(u_traj + (traj_index + num_timesteps) * control_dim); // trajectory u at time t
+      Eigen::Map<DoubleIntegratorDynamics::control_array>
+        eps_t(noisey_sample + (traj_index + num_timesteps) * control_dim); // Noise at time t
+      Eigen::Matrix<float, control_dim, state_dim> feedback_gains_t; // Feedback gains at time t
+
+      // Create newly calculated values at time t in rollout i
+      DoubleIntegratorDynamics::state_array x_dot_t_nom;
+      DoubleIntegratorDynamics::state_array x_dot_t_act;
+      DoubleIntegratorDynamics::control_array u_nom = u_t + eps_t;
+      DoubleIntegratorDynamics::control_array fb_u_t = feedback_gains_t * (x_t_nom - x_t_act);
+      DoubleIntegratorDynamics::control_array u_act = u_nom + fb_u_t;
+      // Dyanamics Update
+      model.computeStateDeriv(x_t_nom, u_nom, x_dot_t_nom);
+      model.computeStateDeriv(x_t_act, u_act, x_dot_t_act);
+
+      // Cost update
+      // cost_real_w_tracking += cost->computeRunningCost(x_t_act.data(), u_t.data(), u_nom_t.data(),);
+
+    }
+  }
 }
 
 /******************************************************************************
