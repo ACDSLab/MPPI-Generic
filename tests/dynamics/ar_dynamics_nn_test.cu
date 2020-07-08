b2da86390abd8e1c752bc382a3f9605d528c6fde
Jason Gibson <jgibson37@gatech.edu>
2020-07-07 20:05:11 -0400

    added test case to verify NN is the same as autorally code

--- tests/dynamics/ar_dynamics_nn_test.cu
+++ tests/dynamics/ar_dynamics_nn_test.cu
@@ -11,11 +11,6 @@
 // Auto-generated header file
 #include <autorally_test_network.h>
 
-//Including neural net model
-#ifdef MPPI_NNET_USING_CONSTANT_MEM__
-__device__ __constant__ float NNET_PARAMS[param_counter(6,32,32,4)];
-#endif
-
 /**
  * Note: the analytical solution for the test NN is outlined in the python script
  */
@@ -588,7 +583,7 @@ TEST(ARNeuralNetDynamics, full) {
     s_der[0] = {0.0};
     u[0] = {0.0};
 
-    launchFullARNNTestKernel<NeuralNetModel<7, 2, 3, 6, 32, 32, 4>, 7, 2>(model, s, u,
+    launchFullARNNTestKernel<NeuralNetModel<7, 2, 3, 6, 32, 32, 4>, 7, 2, 1, 1>(model, s, u,
                                                                           s_der, 0.1, y_dim);
 
     EXPECT_FLOAT_EQ(s[0][0], 0);
@@ -616,7 +611,7 @@ TEST(ARNeuralNetDynamics, full) {
     std::fill(theta.begin(), theta.end(), 1);
     model.updateModel({6, 32, 32, 4}, theta);
 
-    launchFullARNNTestKernel<NeuralNetModel<7, 2, 3, 6, 32, 32, 4>, 7, 2>(model, s, u,
+    launchFullARNNTestKernel<NeuralNetModel<7, 2, 3, 6, 32, 32, 4>, 7, 2, 1>(model, s, u,
                                                                              s_der, 0.1, y_dim);
 
     EXPECT_FLOAT_EQ(s[0][0], 0);
@@ -630,12 +625,190 @@ TEST(ARNeuralNetDynamics, full) {
     EXPECT_FLOAT_EQ(s_der[0][0], 0);
     EXPECT_FLOAT_EQ(s_der[0][1], 0);
     EXPECT_FLOAT_EQ(s_der[0][2], 0);
-    EXPECT_FLOAT_EQ(s_der[0][3], 0);
-    EXPECT_FLOAT_EQ(s_der[0][4], 0);
-    EXPECT_FLOAT_EQ(s_der[0][5], 0);
-    EXPECT_FLOAT_EQ(s_der[0][6], 0);
+    EXPECT_FLOAT_EQ(s_der[0][3], 25.371017);
+    EXPECT_FLOAT_EQ(s_der[0][4], 25.371017);
+    EXPECT_FLOAT_EQ(s_der[0][5], 25.371017);
+    EXPECT_FLOAT_EQ(s_der[0][6], 25.371017);
 
     EXPECT_FLOAT_EQ(u[0][0], 1.0);
     EXPECT_FLOAT_EQ(u[0][1], -2.0);
+  }
+}
+
+void parseTextIntoDataPointHelper(std::string text, std::array<float, 7>& state, std::array<float, 7>& state_result,
+        std::array<float, 7>& state_der, std::array<float, 2>& control) {
+  size_t line_pos = 0;
+  size_t prev_line_pos = 1;
+  int what_var = 0;
+  text.append(" *");
+  while ((line_pos = text.find("*", prev_line_pos)) != std::string::npos) {
+    std::string line = text.substr(prev_line_pos, line_pos - prev_line_pos);
+    line.append(" ");
+    size_t value_pos = 0;
+    size_t prev_value_pos = 0;
+    int counter = 0;
+    while ((value_pos = line.find(" ", prev_value_pos)) != std::string::npos) {
+      std::string value = line.substr(prev_value_pos, value_pos - prev_value_pos);
+      // makes sure it is a number
+      if(!value.empty()) {
+        float number = 0;// = std::stoi(value.substr(0, std::string::npos));
+        if(value[0] == '-' && isdigit(value[1]) || isdigit(value[0]) ) {
+          number = std::stof(value);
+        } else {
+          prev_value_pos = value_pos+1;
+          continue;
+        }
+        if(what_var == 0) {
+          state[counter++] = number;
+        } else if(what_var == 1) {
+          control[counter++] = number;
+        } else if(what_var == 2) {
+          state_der[counter++] = number;
+        } else if(what_var == 3) {
+          state_result[counter++] = number;
+        }
+      }
+      prev_value_pos = std::min(value_pos+1, line.length());
+    }
+    what_var++;
+    prev_line_pos = std::min(line_pos+1, text.length());
+  }
+}
+
+/**
+ * values grabbed 7/7/20 from melodic-devel in gazebo with
+ * model = <param name="model_path" value="$(env AR_MPPI_PARAMS_PATH)/models/autorally_nnet_09_12_2018.npz" />
+ *
+ * input state 4.264431 -30.974377 -0.955451 -0.028595 3.346700 0.048521 0.315486
+ * input control -0.221381 0.089168
+ * output state_der 1.971473 -2.704820 -0.315486 0.136986 -0.877249 0.713279 -2.542408
+ * output state 4.303861 -31.028473 -0.961760 -0.025856 3.329155 0.062786 0.264637
+ *
+ * input state 17.818813 -33.751003 2.603801 0.046806 3.690995 -0.162579 0.046056
+ * input control 0.247343 0.521264
+ * output state_der -3.086702 2.030306 -0.046056 -0.170345 5.059716 -0.795022 1.001305
+ * output state 17.757080 -33.710396 2.602880 0.043399 3.792189 -0.178480 0.066082
+ *
+ * input state 29.102535 -28.311907 0.324347 0.030684 4.354917 -0.250158 -0.181747
+ * input control -0.465928 0.304528
+ * output state_der 4.207570 1.150753 0.181747 0.285242 0.759887 1.638221 -3.125369
+ * output state 29.186687 -28.288891 0.327982 0.036389 4.370114 -0.217393 -0.244254
+ *
+ * input state -2.603741 -15.197432 2.065675 0.001336 4.745344 0.066507 0.039070
+ * input control 0.212821 0.477029
+ * output state_der -2.312211 4.144441 -0.039070 -0.049963 4.542547 -1.031745 1.590496
+ * output state -2.649985 -15.114543 2.064894 0.000337 4.836195 0.045872 0.070880
+ *
+ * input state -2.460799 -15.449237 2.066902 0.001312 4.874847 0.114099 0.022956
+ * input control -0.126225 -0.288144
+ * output state_der -2.420791 4.232839 -0.022956 0.106002 -2.117054 -0.294837 -1.255360
+ * output state -2.509215 -15.364580 2.066442 0.003432 4.832506 0.108203 -0.002152
+ *
+ * input state -9.680823 -7.434339 11.683491 0.428528 -1.655267 -3.769846 -2.194825
+ * input control -0.003010 0.147335
+ * output state_der -3.963449 -1.114773 2.194825 -0.100577 3.684401 7.052448 2.626748
+ * output state -9.760093 -7.456634 11.727387 0.426516 -1.581579 -3.628797 -2.142290
+ *
+ * input state 20.899069 -37.733856 0.468771 0.109863 6.329141 -0.916158 -0.136938
+ * input control 0.281979 0.775381
+ * output state_der 6.060292 2.042114 0.136938 -0.325823 4.262759 -1.176095 0.928013
+ * output state 21.020275 -37.693012 0.471509 0.103346 6.414396 -0.939680 -0.118377
+ */
+TEST(ARNeuralNetDynamics, fullComparedToAutoRallyImpl) {
+  std::array<float2, 2> u_constraint = {};
+  u_constraint[0].x = -0.99;
+  u_constraint[0].y = 0.65;
+  u_constraint[1].x = -0.99;
+  u_constraint[1].y = 0.99;
+
+  NeuralNetModel<7, 2, 3, 6, 32, 32, 4> model(u_constraint);
+
+  model.GPUSetup();
+
+  int number_queries = 7;
+
+  std::vector<std::array<float, 7>> s(number_queries);
+  // x_dot, y_dot, theta_dot
+  std::vector<std::array<float, 7>> s_der(number_queries);
+  // steering, throttle
+  std::vector<std::array<float, 2>> u(number_queries);
+
+  std::vector<std::array<float, 7>> s_result(number_queries);
+  // x_dot, y_dot, theta_dot
+  std::vector<std::array<float, 7>> s_der_result(number_queries);
+  // steering, throttle
+  std::vector<std::array<float, 2>> u_result(number_queries);
+
+  parseTextIntoDataPointHelper(
+          "* input state 4.264431 -30.974377 -0.955451 -0.028595 3.346700 0.048521 0.315486"
+          "* input control -0.221381 0.089168"
+          "* output state_der 1.971473 -2.704820 -0.315486 0.136986 -0.877249 0.713279 -2.542408"
+          "* output state 4.303861 -31.028473 -0.961760 -0.025856 3.329155 0.062786 0.264637",
+                               s[0], s_result[0], s_der_result[0], u[0]);
+  parseTextIntoDataPointHelper(
+          "* input state 17.818813 -33.751003 2.603801 0.046806 3.690995 -0.162579 0.046056"
+          "* input control 0.247343 0.521264"
+          "* output state_der -3.086702 2.030306 -0.046056 -0.170345 5.059716 -0.795022 1.001305"
+          "* output state 17.757080 -33.710396 2.602880 0.043399 3.792189 -0.178480 0.066082",
+                               s[1], s_result[1], s_der_result[1], u[1]);
+  parseTextIntoDataPointHelper(
+          "* input state 29.102535 -28.311907 0.324347 0.030684 4.354917 -0.250158 -0.181747"
+          "* input control -0.465928 0.304528"
+          "* output state_der 4.207570 1.150753 0.181747 0.285242 0.759887 1.638221 -3.125369"
+          "* output state 29.186687 -28.288891 0.327982 0.036389 4.370114 -0.217393 -0.244254",
+                               s[2], s_result[2], s_der_result[2], u[2]);
+  parseTextIntoDataPointHelper(
+          "* input state -2.603741 -15.197432 2.065675 0.001336 4.745344 0.066507 0.039070"
+          "* input control 0.212821 0.477029"
+          "* output state_der -2.312211 4.144441 -0.039070 -0.049963 4.542547 -1.031745 1.590496"
+          "* output state -2.649985 -15.114543 2.064894 0.000337 4.836195 0.045872 0.070880",
+                               s[3], s_result[3], s_der_result[3], u[3]);
+  parseTextIntoDataPointHelper(
+          "* input state -2.460799 -15.449237 2.066902 0.001312 4.874847 0.114099 0.022956"
+          "* input control -0.126225 -0.288144"
+          "* output state_der -2.420791 4.232839 -0.022956 0.106002 -2.117054 -0.294837 -1.255360"
+          "* output state -2.509215 -15.364580 2.066442 0.003432 4.832506 0.108203 -0.002152",
+                               s[4], s_result[4], s_der_result[4], u[4]);
+  parseTextIntoDataPointHelper(
+          "* input state -9.680823 -7.434339 11.683491 0.428528 -1.655267 -3.769846 -2.194825"
+          "* input control -0.003010 0.147335"
+          "* output state_der -3.963449 -1.114773 2.194825 -0.100577 3.684401 7.052448 2.626748"
+          "* output state -9.760093 -7.456634 11.727387 0.426516 -1.581579 -3.628797 -2.142290",
+                               s[5], s_result[5], s_der_result[5], u[5]);
+  parseTextIntoDataPointHelper(
+          "* input state 20.899069 -37.733856 0.468771 0.109863 6.329141 -0.916158 -0.136938"
+          "* input control 0.281979 0.775381"
+          "* output state_der 6.060292 2.042114 0.136938 -0.325823 4.262759 -1.176095 0.928013"
+          "* output state 21.020275 -37.693012 0.471509 0.103346 6.414396 -0.939680 -0.118377",
+                              s[6], s_result[6], s_der_result[6], u[6]);
+
+
+  std::copy(u.begin(), u.end(), u_result.begin());
+
+  model.loadParams("/home/jason/catkin_ws/ar/src/autorally/"
+                   "autorally_control/src/path_integral/params/models/autorally_nnet_09_12_2018.npz");
+
+  for(int y_dim = 1; y_dim < 33; y_dim++) {
+    std::vector<std::array<float, 7>> s_temp(s);
+    std::vector<std::array<float, 7>> s_der_temp(s_der);
+    std::vector<std::array<float, 2>> u_temp(u);
+
+    launchFullARNNTestKernel<NeuralNetModel<7, 2, 3, 6, 32, 32, 4>, 7, 2, 7>(model, s_temp, u_temp,
+                                                                          s_der_temp, 1.0/50, y_dim);
+
+    for(int point_index = 0; point_index < 1; point_index++) {
+        for(int dim = 0; dim < 7; dim++) {
+          EXPECT_NEAR(s_temp[point_index][dim], s_result[point_index][dim], 0.001)
+          << "point_index: " << point_index << " dim: " << dim << " y_dim: " << y_dim <<
+          " diff = " << std::abs(s_temp[point_index][dim] - s_result[point_index][dim]);
+          EXPECT_NEAR(s_der_temp[point_index][dim], s_der_result[point_index][dim], 0.001)
+          << "point_index: " << point_index << " dim: " << dim << " y_dim: " << y_dim <<
+          " diff = " << std::abs(s_der_temp[point_index][dim] - s_der_result[point_index][dim]);
+        }
+        for(int dim = 0; dim < 2; dim++) {
+          EXPECT_NEAR(u_temp[point_index][dim], u_result[point_index][dim], 0.001)
+          << "point_index: " << point_index << " dpoint_indexm: " << dim;
+        }
+    }
   }
 }
