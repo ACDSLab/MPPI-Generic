70b4c9500355410941f7006512f777ec66f6415d
Bogdan Vlahov <bvlahov3@gatech.edu>
2020-07-22 10:52:58 -0400

    WIP Add quadrotor map cost class skeleton

---
+++ include/mppi/cost_functions/quadrotor/quadrotor_map_cost.cuh
@@ -0,0 +1,37 @@
+/*
+ * Created on Wed Jul 22 2020 by Bogdan
+ */
+#ifndef MPPI_COST_FUNCTIONS_QUADROTOR_MAP_COST_CUH_
+#define MPPI_COST_FUNCTIONS_QUADROTOR_MAP_COST_CUH_
+
+#include <mppi/cost_functions/cost.cuh>
+#include <mppi/utils/math_utils.h>
+
+struct QuadrotorMapCostParams : public CostParams<4> {
+  QuadrotorMapCostParams() {
+    control_cost_coeff[0] = 1;  // roll
+    control_cost_coeff[1]] = 1; // pitch
+    control_cost_coeff[2] = 1;  // yaw
+    control_cost_coeff[3] = 1;  // throttle
+  }
+}
+
+template <class CLASS_T, class PARAMS_T = QuadrotorMapCostParams>
+class QuadrotorMapCostImpl : public Cost<CLASS_T, PARAMS_T, 13, 4> {
+public:
+  QuadrotorMapCostImpl(cudaStream_t stream = 0) {
+    // do stuff
+  }
+protected:
+  std::string map_path_;
+}
+
+class QuadrotorMapCost : public QuadrotorMapCostImpl<QuadrotorMapCost> {
+public:
+  QuadrotorMapCost(cudaStream_t stream = 0) : QuadrotorMapCostImpl<QuadrotorMapCost>(stream) {};
+}
+
+#if __CUDACC__
+#include "quadrotor_map_cost.cu"
+#endif
+#endif  // MPPI_COST_FUNCTIONS_QUADROTOR_MAP_COST_CUH_
