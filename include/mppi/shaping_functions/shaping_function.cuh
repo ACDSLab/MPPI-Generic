b443df72f264104ccbe236c3e4ac37815218f917
Jason Gibson <jgibson37@gatech.edu>
2021-03-11 16:42:08 -0500

    initial commit of the norm exponential kernel becoming a class

---
+++ include/mppi/shaping_functions/shaping_function.cuh
@@ -0,0 +1,68 @@
+#ifndef MPPIGENERIC_SHAPING_FUNCTION_CUH
+#define MPPIGENERIC_SHAPING_FUNCTION_CUH
+
+#include <mppi/core/mppi_common.cuh>
+#include <mppi/utils/managed.cuh>
+
+namespace mppi_common {
+  template<class CLASS_T, int NUM_ROLLOUTS>
+  __global__ void weightKernel(float* trajectory_costs_d,
+                               float baseline,
+                               CLASS_T* shape_function);
+}
+
+struct ShapingFunctionParams {
+  float lambda_inv = 1.0; // also known as gamma
+};
+
+template<class CLASS_T, class PARAMS_T, int NUM_ROLLOUTS, int BDIM_X>
+class ShapingFunctionImpl : public Managed {
+public:
+
+  typedef Eigen::Matrix<float, NUM_ROLLOUTS, 1> cost_traj;
+
+  ShapingFunctionImpl() {
+    paramsToDevice();
+  }
+
+  __host__ __device__ float computeWeight(float* traj_cost, float baseline, int global_idx);
+
+  void computeWeights(cost_traj& trajectory_costs, float* trajectory_costs_d, cudaStream_t stream=nullptr);
+
+  void launchWeightKernel(float* trajectory_costs_d, float baseline, cudaStream_t stream=nullptr);
+
+  void computeFreeEnergy(float& free_energy, float& free_energy_var,
+                         float& free_energy_modified,
+                         float* cost_rollouts_host, float baseline);
+
+  PARAMS_T getParams() {return params_;}
+  float getBaseline(){return baseline_;}
+  float getNormalizer(){return normalizer_;}
+
+  void setParams(PARAMS_T params) {
+    this->params_ = params;
+    paramsToDevice();
+  }
+
+  void GPUSetup();
+  void freeCudaMem();
+  void paramsToDevice();
+
+  CLASS_T* shaping_function_d_ = nullptr;
+
+private:
+  PARAMS_T params_;
+  float baseline_;
+  float normalizer_;
+};
+
+template<int NUM_ROLLOUTS, int BDIM_X>
+class ShapingFunction : public ShapingFunctionImpl<ShapingFunction<NUM_ROLLOUTS, BDIM_X>, ShapingFunctionParams, NUM_ROLLOUTS, BDIM_X> {
+public:
+};
+
+#if __CUDACC__
+#include "shaping_function.cu"
+#endif
+
+#endif //MPPIGENERIC_SHAPING_FUNCTION_CUH
---
+++ tests/shaping_functions/shaping_function_test.cu
@@ -0,0 +1,170 @@
+#include <gtest/gtest.h>
+#include <mppi/dynamics/cartpole/cartpole_dynamics.cuh>
+#include <mppi/cost_functions/cartpole/cartpole_quadratic_cost.cuh>
+
+#include <mppi/utils/test_helper.h>
+#include <random>
+
+#include <mppi/shaping_functions/shaping_function.cuh>
+
+class ShapingFunctionTest: public testing::Test {
+protected:
+  void SetUp() override {
+    generator = std::default_random_engine(7.0);
+    distribution = std::normal_distribution<float>(100.0,2.0);
+  }
+
+  void TearDown() override {
+
+  }
+
+  std::default_random_engine generator;
+  std::normal_distribution<float> distribution;
+};
+
+TEST_F(ShapingFunctionTest, computeWeightTest) {
+  const int num_rollouts = 500;
+  std::array<float, num_rollouts> cost_vec = {0};
+  ShapingFunctionParams params;
+  ShapingFunction<num_rollouts, 1> shaping_function;
+
+  // Use a range based for loop to set the cost
+  for (auto &cost: cost_vec) {
+    cost = distribution(generator);
+  }
+
+  float min_cost_known = *std::min_element(cost_vec.begin(), cost_vec.end());
+
+  for (float lambda_inv = 0.0; lambda_inv < 3.0; lambda_inv += 0.1) {
+    params.lambda_inv = lambda_inv;
+    shaping_function.setParams(params);
+    for (int i = 0; i < cost_vec.size(); i++) {
+      float weight = shaping_function.computeWeight(cost_vec.data(), min_cost_known, i);
+      EXPECT_FLOAT_EQ(weight, expf(-lambda_inv * (cost_vec[i] - min_cost_known)));
+    }
+  }
+}
+
+template<class CLASS_T, int NUM_ROLLOUTS, int BDIM_X>
+void launchShapingFunction_KernelTest(std::array<float, NUM_ROLLOUTS>& trajectory_costs_host, CLASS_T& shape_function, float baseline, std::array<float, NUM_ROLLOUTS>& normalized_compute) {
+  // Allocate CUDA memory
+  float* trajectory_costs_d;
+  HANDLE_ERROR(cudaMalloc((void**)&trajectory_costs_d, sizeof(float)*trajectory_costs_host.size()))
+
+  HANDLE_ERROR(cudaMemcpy(trajectory_costs_d, trajectory_costs_host.data(), sizeof(float)*trajectory_costs_host.size(), cudaMemcpyHostToDevice))
+
+  mppi_common::weightKernel<CLASS_T, NUM_ROLLOUTS><<<1,NUM_ROLLOUTS>>>(trajectory_costs_d, baseline, shape_function.shaping_function_d_);
+  CudaCheckError();
+
+  HANDLE_ERROR(cudaMemcpy(normalized_compute.data(), trajectory_costs_d, sizeof(float)*trajectory_costs_host.size(), cudaMemcpyDeviceToHost))
+
+  cudaFree(trajectory_costs_d);
+}
+
+TEST_F(ShapingFunctionTest, weightKernelTest) {
+  const int num_rollouts = 500;
+  std::array<float, num_rollouts> cost_vec = {0};
+  std::array<float, num_rollouts> result_cost_vec = {0};
+  ShapingFunctionParams params;
+  ShapingFunction<num_rollouts, 1> shaping_function;
+  shaping_function.GPUSetup();
+
+  // Use a range based for loop to set the cost
+  for (auto &cost: cost_vec) {
+    cost = distribution(generator);
+  }
+
+  float min_cost_known = *std::min_element(cost_vec.begin(), cost_vec.end());
+
+  for (float lambda_inv = 0.01; lambda_inv < 3.0; lambda_inv += 0.1) {
+    params.lambda_inv = lambda_inv;
+    shaping_function.setParams(params);
+    launchShapingFunction_KernelTest<ShapingFunction<num_rollouts, 1>, num_rollouts, 1>(cost_vec, shaping_function, min_cost_known, result_cost_vec);
+    for (int i = 0; i < cost_vec.size(); i++) {
+      EXPECT_FLOAT_EQ(result_cost_vec[i], expf(-lambda_inv * (cost_vec[i] - min_cost_known)));
+    }
+  }
+}
+
+template<class CLASS_T, int NUM_ROLLOUTS>
+void launchShapingFunction_KernelTest(typename CLASS_T::cost_traj& trajectory_costs_host, CLASS_T& shape_function,
+                                      float baseline, std::array<float, NUM_ROLLOUTS>& normalized_compute,
+                                      cudaStream_t stream=nullptr) {
+  // Allocate CUDA memory
+  float* trajectory_costs_d;
+  HANDLE_ERROR(cudaMalloc((void**)&trajectory_costs_d, sizeof(float)*trajectory_costs_host.size()))
+
+  HANDLE_ERROR(cudaMemcpy(trajectory_costs_d, trajectory_costs_host.data(), sizeof(float)*trajectory_costs_host.size(), cudaMemcpyHostToDevice))
+
+  shape_function.launchWeightKernel(trajectory_costs_d, baseline, stream);
+  CudaCheckError();
+
+  HANDLE_ERROR(cudaMemcpy(normalized_compute.data(), trajectory_costs_d, sizeof(float)*trajectory_costs_host.size(), cudaMemcpyDeviceToHost))
+
+  cudaFree(trajectory_costs_d);
+}
+
+TEST_F(ShapingFunctionTest, launchWeightKernelTest) {
+  const int num_rollouts = 500;
+  ShapingFunction<num_rollouts, 1>::cost_traj cost_traj;
+  std::array<float, num_rollouts> result_cost_vec = {0};
+
+  ShapingFunctionParams params;
+  ShapingFunction<num_rollouts, 1> shaping_function;
+  shaping_function.GPUSetup();
+
+  // Use a range based for loop to set the cost
+  cost_traj = ShapingFunction<num_rollouts, 1>::cost_traj::Zero();
+  for (int i = 0; i < num_rollouts; i++) {
+    cost_traj(i) = distribution(generator);
+  }
+
+  float min_cost_known = cost_traj.minCoeff();
+
+  for (float lambda_inv = 0.01; lambda_inv < 3.0; lambda_inv += 0.1) {
+    params.lambda_inv = lambda_inv;
+    shaping_function.setParams(params);
+    launchShapingFunction_KernelTest<ShapingFunction<num_rollouts, 1>, num_rollouts>(cost_traj, shaping_function, min_cost_known, result_cost_vec);
+    for (int i = 0; i < result_cost_vec.size(); i++) {
+      EXPECT_FLOAT_EQ(result_cost_vec[i], expf(-lambda_inv * (cost_traj(i) - min_cost_known)));
+    }
+  }
+}
+
+
+TEST_F(ShapingFunctionTest, computeWeightsTest) {
+  const int num_rollouts = 500;
+  ShapingFunction<num_rollouts, 1>::cost_traj cost_traj;
+  ShapingFunction<num_rollouts, 1>::cost_traj result_cost_traj;
+
+  ShapingFunctionParams params;
+  ShapingFunction<num_rollouts, 1> shaping_function;
+  shaping_function.GPUSetup();
+
+  // Use a range based for loop to set the cost
+  cost_traj = ShapingFunction<num_rollouts, 1>::cost_traj::Zero();
+  for (int i = 0; i < num_rollouts; i++) {
+    cost_traj(i) = distribution(generator);
+    result_cost_traj(i) = cost_traj(i);
+  }
+
+  float* trajectory_costs_d;
+  HANDLE_ERROR(cudaMalloc((void**)&trajectory_costs_d, sizeof(float)*num_rollouts));
+  HANDLE_ERROR(cudaMemcpy(trajectory_costs_d, cost_traj.data(), sizeof(float)*cost_traj.size(), cudaMemcpyHostToDevice))
+
+  for (float lambda_inv = 0.01; lambda_inv < 3.0; lambda_inv += 0.1) {
+    float min_cost_known = cost_traj.minCoeff();
+
+    params.lambda_inv = lambda_inv;
+    shaping_function.setParams(params);
+    shaping_function.computeWeights(cost_traj, trajectory_costs_d);
+    float normalizer = cost_traj.sum();
+
+    EXPECT_EQ(shaping_function.getBaseline(), min_cost_known);
+    EXPECT_EQ(shaping_function.getNormalizer(), normalizer);
+    for (int i = 0; i < cost_traj.size(); i++) {
+      EXPECT_FLOAT_EQ(cost_traj(i), expf(-lambda_inv * (result_cost_traj(i) - min_cost_known)));
+      result_cost_traj(i) = cost_traj(i);
+    }
+  }
+}
