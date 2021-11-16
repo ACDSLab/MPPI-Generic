1cbd749b5eb6e9be09797c7ffa55cc5ec006a6c4
Jason Gibson <jgibson37@gatech.edu>
2021-11-15 20:02:35 -0500

    added dubins dynamics model

---
+++ include/mppi/dynamics/dubins/dubins.cu
@@ -0,0 +1,56 @@
+#include <mppi/dynamics/dubins/dubins.cuh>
+
+DubinsDynamics::DubinsDynamics(cudaStream_t stream) :
+        Dynamics<DubinsDynamics, DubinsParams, 3, 2>(stream) {
+  this->params_ = DubinsParams();
+}
+
+void DubinsDynamics::computeDynamics(const Eigen::Ref<const state_array> &state,
+                                               const Eigen::Ref<const control_array> &control, Eigen::Ref<state_array> state_der) {
+  state_der(0) = control(0)*cos(state(2));
+  state_der(1) = control(0)*sin(state(2));
+  state_der(2) = control(1);
+}
+
+bool DubinsDynamics::computeGrad(const Eigen::Ref<const state_array> &state,
+                                           const Eigen::Ref<const control_array> &control,
+                                           Eigen::Ref<dfdx> A,
+                                           Eigen::Ref<dfdu> B) {
+  return false;
+}
+
+void DubinsDynamics::updateState(Eigen::Ref<state_array> state,
+                 Eigen::Ref<state_array> state_der, const float dt) {
+  state += state_der * dt;
+  state(2) = angle_utils::normalizeAngle(state(2));
+  state_der.setZero();
+}
+
+DubinsDynamics::state_array DubinsDynamics::interpolateState(const Eigen::Ref<state_array> state_1,
+                             const Eigen::Ref<state_array> state_2, const double alpha) {
+  state_array result = (1 - alpha)*state_1 + alpha*state_2;
+  result(2) = angle_utils::interpolateEulerAngleLinear(state_1(2), state_2(2), alpha);
+  return result;
+}
+
+__device__ void DubinsDynamics::updateState(float* state, float* state_der, const float dt) {
+  int i;
+  int tdy = threadIdx.y;
+  //Add the state derivative time dt to the current state.
+  //printf("updateState thread %d, %d = %f, %f\n", threadIdx.x, threadIdx.y, state[0], state_der[0]);
+  for (i = tdy; i < STATE_DIM; i+=blockDim.y){
+    state[i] += state_der[i]*dt;
+    if (tdy == 2) {
+      state[i] = angle_utils::normalizeAngle(state[i]);
+    }
+    state_der[i] = 0; //Important: reset the state derivative to zero.
+  }
+}
+
+__device__ void DubinsDynamics::computeDynamics(float* state, float* control,
+                                                          float* state_der, float* theta_s)
+{
+  state_der[0] = control[0]*cos(state[2]);
+  state_der[1] = control[0]*sin(state[2]);
+  state_der[2] = control[1];
+}
---
+++ tests/dynamics/dubins_dynamics_tests.cu
@@ -0,0 +1,172 @@
+#include <Eigen/Dense>
+#include <gtest/gtest.h>
+#include <mppi/dynamics/dubins/dubins.cuh>
+#include <mppi/dynamics/dynamics_generic_kernel_tests.cuh>
+#include <cuda_runtime.h>
+
+TEST(DubinsDynamics, Template) {
+  auto dynamics = DubinsDynamics();
+  EXPECT_EQ(3, DubinsDynamics::STATE_DIM);
+  EXPECT_EQ(2, DubinsDynamics::CONTROL_DIM);
+}
+
+TEST(DubinsDynamics, BindStream) {
+  cudaStream_t stream;
+
+  HANDLE_ERROR(cudaStreamCreate(&stream));
+
+  auto dynamics = DubinsDynamics(stream);
+
+  EXPECT_EQ(dynamics.stream_, stream) << "Stream binding failure.";
+
+  HANDLE_ERROR(cudaStreamDestroy(stream));
+}
+
+TEST(DubinsDynamics, ComputeDynamics) {
+  DubinsDynamics dynamics = DubinsDynamics();
+  DubinsDynamics::state_array x;
+  x << 0, 0, 0;
+  DubinsDynamics::control_array u;
+  u << 0, 0;
+
+  DubinsDynamics::state_array next_x;
+  dynamics.computeDynamics(x, u, next_x);
+  EXPECT_FLOAT_EQ(next_x(0), 0);
+  EXPECT_FLOAT_EQ(next_x(1), 0);
+  EXPECT_FLOAT_EQ(next_x(2), 0);
+
+  x << 1, 2, 0;
+  u << 1, 0;
+  dynamics.computeDynamics(x, u, next_x);
+  EXPECT_FLOAT_EQ(next_x(0), 1);
+  EXPECT_FLOAT_EQ(next_x(1), 0);
+  EXPECT_FLOAT_EQ(next_x(2), 0);
+
+  x << 1, 2, 0;
+  u << 3, 1;
+  dynamics.computeDynamics(x, u, next_x);
+  EXPECT_FLOAT_EQ(next_x(0), 3);
+  EXPECT_FLOAT_EQ(next_x(1), 0);
+  EXPECT_FLOAT_EQ(next_x(2), 1);
+
+  x << 1, 2, M_PI_2;
+  u << 4, 1;
+  dynamics.computeDynamics(x, u, next_x);
+  EXPECT_NEAR(next_x(0), 0.0, 1e-5);
+  EXPECT_FLOAT_EQ(next_x(1), 4*sin(M_PI_2));
+  EXPECT_FLOAT_EQ(next_x(2), 1);
+
+  // TODO test case for flipping across angle discontinuity
+  x << 1, 2, M_PI_2;
+  u << 4, 1;
+  dynamics.computeDynamics(x, u, next_x);
+  EXPECT_NEAR(next_x(0), 0.0, 1e-5);
+  EXPECT_FLOAT_EQ(next_x(1), 4*sin(M_PI_2));
+  EXPECT_FLOAT_EQ(next_x(2), 1);
+}
+
+TEST(DubinsDynamics, TestDynamicsGPU) {
+  DubinsDynamics dynamics = DubinsDynamics();
+  dynamics.GPUSetup();
+
+  DubinsDynamics::state_array state;
+  state(0) = 0.5;
+  state(1) = 0.7;
+  state(2) = M_PI_4;
+  DubinsDynamics::control_array control;
+  control(0) = 3.0;
+  control(1) = 2.0;
+
+  std::vector<std::array<float, 3>> s(1);
+  for(int dim = 0; dim < 3; dim++) {
+    s[0][dim] = state(dim);
+  }
+  std::vector<std::array<float, 3>> s_der(1);
+  // steering, throttle
+  std::vector<std::array<float, 2>> u(1);
+  for(int dim = 0; dim < 2; dim++) {
+    u[0][dim] = control(dim);
+  }
+
+  // These variables will be changed so initialized to the right size only
+  DubinsDynamics::state_array state_der_cpu = DubinsDynamics::state_array::Zero();
+
+  // Run dynamics on dynamicsU
+  dynamics.computeDynamics(state, control, state_der_cpu);
+  // Run dynamics on GPU
+  for(int y_dim = 1; y_dim <= 3; y_dim++) {
+    launchComputeDynamicsTestKernel<DubinsDynamics, 3, 2>(dynamics, s, u, s_der, y_dim);
+    for(int dim = 0; dim < 3; dim++) {
+      EXPECT_FLOAT_EQ(state_der_cpu(dim), s_der[0][dim]);
+    }
+  }
+
+  dynamics.freeCudaMem();
+}
+
+TEST(DubinsDynamics, TestUpdateStateGPU) {
+  DubinsDynamics dynamics = DubinsDynamics();
+  dynamics.GPUSetup();
+
+  DubinsDynamics::state_array state;
+  state(0) = 0.5;
+  state(1) = 0.7;
+  state(2) = M_PI;
+  DubinsDynamics::control_array control;
+  control(0) = 3.0;
+  control(1) = 2.0;
+
+  std::vector<std::array<float, 3>> s(1);
+  for(int dim = 0; dim < 3; dim++) {
+    s[0][dim] = state(dim);
+  }
+  std::vector<std::array<float, 3>> s_der(1);
+  // steering, throttle
+  std::vector<std::array<float, 2>> u(1);
+  for(int dim = 0; dim < 2; dim++) {
+    u[0][dim] = control(dim);
+  }
+
+  // These variables will be changed so initialized to the right size only
+  DubinsDynamics::state_array state_der_cpu = DubinsDynamics::state_array::Zero();
+
+  // Run dynamics on dynamicsU
+  dynamics.computeDynamics(state, control, state_der_cpu);
+  dynamics.updateState(state, state_der_cpu, 0.1f);
+  // Run dynamics on GPU
+  for(int y_dim = 1; y_dim <= 3; y_dim++) {
+    launchComputeStateDerivTestKernel<DubinsDynamics, 3, 2>(dynamics, s, u, s_der, y_dim);
+    launchUpdateStateTestKernel<DubinsDynamics, 3>(dynamics, s, s_der, 0.1f, y_dim);
+    for(int dim = 0; dim < 3; dim++) {
+      EXPECT_FLOAT_EQ(state_der_cpu(dim), s_der[0][dim]);
+    }
+  }
+  dynamics.freeCudaMem();
+}
+
+//TEST(DubinsDynamics, TestComputeGradComputation) {
+//  Eigen::Matrix<float, DubinsDynamics::STATE_DIM, DubinsDynamics::STATE_DIM + DubinsDynamics::CONTROL_DIM> numeric_jac;
+//  Eigen::Matrix<float, DubinsDynamics::STATE_DIM, DubinsDynamics::STATE_DIM + DubinsDynamics::CONTROL_DIM> analytic_jac;
+//  DubinsDynamics::state_array state;
+//  state << 1, 2, 3, 4;
+//  DubinsDynamics::control_array control;
+//  control << 5;
+//
+//  auto analytic_grad_model = DubinsDynamics(1,1,1);
+//
+//  DubinsDynamics::dfdx A_analytic = DubinsDynamics::dfdx::Zero();
+//  DubinsDynamics::dfdu B_analytic = DubinsDynamics::dfdu::Zero();
+//
+//  analytic_grad_model.computeGrad(state, control, A_analytic, B_analytic);
+//
+//  auto numerical_grad_model = DubinsDummy(1,1,1);
+//
+//  std::shared_ptr<ModelWrapperDDP<DubinsDummy>> ddp_model = std::make_shared<ModelWrapperDDP<DubinsDummy>>(&numerical_grad_model);
+//
+//  analytic_jac.leftCols<DubinsDynamics::STATE_DIM>() = A_analytic;
+//  analytic_jac.rightCols<DubinsDynamics::CONTROL_DIM>() = B_analytic;
+//  numeric_jac = ddp_model->df(state, control);
+//
+//  ASSERT_LT((numeric_jac - analytic_jac).norm(), 1e-3) << "Numeric Jacobian\n" << numeric_jac << "\nAnalytic Jacobian\n" << analytic_jac;
+//}
+//
