1fd696074b6b1c294d6f694ebcc5af834fedec59
Jason Gibson <jgibson37@gatech.edu>
2021-11-15 20:02:35 -0500

    added dubins dynamics model

---
+++ include/mppi/dynamics/dubins/dubins.cuh
@@ -0,0 +1,51 @@
+#pragma once
+
+#ifndef DUBINS_CUH_
+#define DUBINS_CUH_
+
+#include <mppi/dynamics/dynamics.cuh>
+#include <mppi/dynamics/angle_utils.cuh>
+#include <random>
+
+struct DubinsParams{
+  DubinsParams() = default;
+  ~DubinsParams() = default;
+};
+
+using namespace MPPI_internal;
+
+class DubinsDynamics : public Dynamics<DubinsDynamics, DubinsParams, 3, 2>
+{
+public:
+  DubinsDynamics(cudaStream_t stream = nullptr);
+
+  void computeDynamics(const Eigen::Ref<const state_array> &state,
+                       const Eigen::Ref<const control_array> &control,
+                       Eigen::Ref<state_array> state_der);
+
+  void updateState(Eigen::Ref<state_array> state,
+                   Eigen::Ref<state_array> state_der, const float dt);
+
+  __device__ void updateState(float* state, float* state_der, const float dt);
+
+  state_array interpolateState(const Eigen::Ref<state_array> state_1,
+                               const Eigen::Ref<state_array> state_2, const double alpha);
+
+    bool computeGrad(const Eigen::Ref<const state_array> & state,
+                   const Eigen::Ref<const control_array>& control,
+                   Eigen::Ref<dfdx> A,
+                   Eigen::Ref<dfdu> B);
+
+  __device__ void computeDynamics(float* state,
+                                  float* control,
+                                  float* state_der,
+                                  float* theta = nullptr);
+private:
+
+};
+
+#if __CUDACC__
+#include "dubins.cu"
+#endif
+
+#endif //!DUBINS_CUH_
