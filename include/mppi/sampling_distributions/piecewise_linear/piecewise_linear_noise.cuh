c0267d73d740a11c457e75a96619bb2d5dbdf530
David Fan <david.fan@gmail.com>
2022-04-16 12:51:40 -0700

    add variable number of random segments

--- include/mppi/sampling_distributions/piecewise_linear/piecewise_linear_noise.cuh
+++ include/mppi/sampling_distributions/piecewise_linear/piecewise_linear_noise.cuh
@@ -14,7 +14,7 @@
 
 __global__ void createPiecewiseLinearNoise(const int num_timesteps, const int num_trajectories, const int control_dim,
       const int num_piecewise_segments, const float scale_noise_factor, const float frac_random_noise_traj,
-      const float* switch_times, const float* switch_values, const float* nominal_control, const float* random_noise, const float* control_std_dev, float* output){
+      const unsigned int* switch_num, const float* switch_times, const float* switch_values, const float* nominal_control, const float* random_noise, const float* control_std_dev, float* output){
   int sample_index = blockIdx.x * blockDim.x + threadIdx.x;
   int time_index = blockIdx.y * blockDim.y + threadIdx.y;
   int control_index = blockIdx.z * blockDim.z + threadIdx.z;
@@ -36,12 +36,14 @@ __global__ void createPiecewiseLinearNoise(const int num_timesteps, const int nu
     // all others, use piecewise linear noise.
     
     // determine start/stop times and piecewise fraction
+    int switch_num_val = switch_num[(sample_index * num_timesteps + time_index) * control_dim + control_index];
+    switch_num_val = min(switch_num_val, num_piecewise_segments);
     int segment_index = 0;
     float start_time = 0.0f;
     float end_time = 1.0f;
     float time_frac = float(time_index) / float(num_timesteps);
 
-    for (int i = 0; i < num_piecewise_segments; i++){
+    for (int i = 0; i < switch_num_val; i++){
       float switch_time = switch_times[(sample_index * (num_piecewise_segments + 1) + i) * control_dim + control_index];
       if (switch_time < time_frac){
         segment_index += 1;
@@ -80,14 +82,20 @@ void piecewise_linear_noise(const int num_timesteps, const int num_trajectories,
   const int BLOCKSIZE_Y = 32;
   const int BLOCKSIZE_Z = 1;
 
+  // try to sample num_piecewise_segments with reasonable poisson distribution
+  const float switch_num_poisson_lambda = 0.5f * float(num_piecewise_segments);
+
   // create random start/stop times and values
   // curandSetStream(gen, stream);
+  unsigned int* switch_num_d;
   float* switch_times_d;
   float* switch_values_d;
   float* random_noise_d;
+  HANDLE_ERROR(cudaMalloc((void**)&switch_num_d, sizeof(unsigned int) * num_trajectories * control_dim));
   HANDLE_ERROR(cudaMalloc((void**)&switch_times_d, sizeof(float) * num_trajectories * (num_piecewise_segments + 1) * control_dim));
   HANDLE_ERROR(cudaMalloc((void**)&switch_values_d, sizeof(float) * num_trajectories * (num_piecewise_segments + 1) * control_dim));
   HANDLE_ERROR(cudaMalloc((void**)&random_noise_d, sizeof(float) * num_trajectories * num_timesteps * control_dim));
+  HANDLE_CURAND_ERROR(curandGeneratePoisson(gen, (unsigned int*)switch_num_d, num_trajectories * control_dim, switch_num_poisson_lambda));
   HANDLE_CURAND_ERROR(curandGenerateUniform(gen, (float*)switch_times_d, num_trajectories * (num_piecewise_segments + 1) * control_dim));
   HANDLE_CURAND_ERROR(curandGenerateUniform(gen, (float*)switch_values_d, num_trajectories * (num_piecewise_segments + 1) * control_dim));
   HANDLE_CURAND_ERROR(curandGenerateNormal(gen, (float*)random_noise_d, num_trajectories * num_timesteps * control_dim, 0.0f, 1.0f));
@@ -99,7 +107,7 @@ void piecewise_linear_noise(const int num_timesteps, const int num_trajectories,
   dim3 grid(grid_x, grid_y, grid_z);
   dim3 block(BLOCKSIZE_X, BLOCKSIZE_Y, BLOCKSIZE_Z);
   createPiecewiseLinearNoise<<<grid, block, 0, stream>>>(num_timesteps, num_trajectories, control_dim,
-                                          num_piecewise_segments, scale_noise_factor, frac_random_noise_traj, switch_times_d, switch_values_d, control_d, random_noise_d, control_std_dev_d, control_noise_d);
+                                          num_piecewise_segments, scale_noise_factor, frac_random_noise_traj, switch_num_d, switch_times_d, switch_values_d, control_d, random_noise_d, control_std_dev_d, control_noise_d);
   
   // cleanup
   HANDLE_ERROR(cudaGetLastError());
