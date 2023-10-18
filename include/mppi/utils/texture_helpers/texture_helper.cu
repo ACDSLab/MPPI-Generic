ca7d8136d4b6591f34a3a2e8e3b9418632da4025
Bogdan Vlahov <bvlahov3@gatech.edu>
2023-10-18 08:49:41 -0400

    Add border as option for out of bounds CUDA tex
    This allows us to set the out of bounds regions of maps to have a
    specific value. Small bug found is that setting the update_mem variable
    in the texture_helper when calling updateAddressMode() or updateBorder()
    causes MPPI to crash

--- include/mppi/utils/texture_helpers/texture_helper.cu
+++ include/mppi/utils/texture_helpers/texture_helper.cu
@@ -359,14 +359,15 @@ void TextureHelper<TEX_T, DATA_T>::updateAddressMode(int index, cudaTextureAddre
   this->textures_buffer_[index].texDesc.addressMode[0] = mode;
   this->textures_buffer_[index].texDesc.addressMode[1] = mode;
   this->textures_buffer_[index].texDesc.addressMode[2] = mode;
-  this->textures_buffer_[index].update_mem = true;
+  // this->textures_buffer_[index].update_mem = true;
 }
 
 template <class TEX_T, class DATA_T>
 void TextureHelper<TEX_T, DATA_T>::updateAddressMode(int index, int layer, cudaTextureAddressMode mode)
 {
   this->textures_buffer_[index].texDesc.addressMode[layer] = mode;
-  this->textures_buffer_[index].update_mem = true;
+  // TODO: seting update mem to true causes crashes. Needs more investigation
+  // this->textures_buffer_[index].update_mem = true;
 }
 
 template <class TEX_T, class DATA_T>
@@ -376,12 +377,14 @@ void TextureHelper<TEX_T, DATA_T>::updateBorder(int index, float value)
   this->textures_buffer_[index].texDesc.borderColor[1] = value;
   this->textures_buffer_[index].texDesc.borderColor[2] = value;
   this->textures_buffer_[index].texDesc.borderColor[3] = value;
-  this->textures_buffer_[index].update_mem = true;
+  // TODO: seting update mem to true causes crashes. Needs more investigation
+  // this->textures_buffer_[index].update_mem = true;
 }
 
 template <class TEX_T, class DATA_T>
 void TextureHelper<TEX_T, DATA_T>::updateBorder(int index, int layer, float value)
 {
   this->textures_buffer_[index].texDesc.borderColor[layer] = value;
-  this->textures_buffer_[index].update_mem = true;
+  // TODO: seting update mem to true causes crashes. Needs more investigation
+  // this->textures_buffer_[index].update_mem = true;
 }
